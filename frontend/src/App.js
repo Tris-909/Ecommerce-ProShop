3af3ccd12997e5421c450c14b09384b065d315fc
= <tranminhtri9090@gmail.com>
2020-11-09 05:04:11 +1000

    Setting Up React-Boostrap, Boostwatch, FontAwesome and create Header, Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-      123
-    </div>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Main Page of the websites</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Navbar, Nav, Container} from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+            <Container>
+                <Navbar.Brand href="/">MERN Shops</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="ml-auto">
+                    <Nav.Link href="/cart">
+                        <i className='fas fa-shopping-cart'></i>
+                        Cart
+                    </Nav.Link>
+                    <Nav.Link href="/login">
+                        <i className='fas fa-user'></i>
+                        Sign In
+                    </Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
