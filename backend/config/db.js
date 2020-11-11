3b40e374a08557b470fec0abed4ac9aa064ed637
= <tranminhtri9090@gmail.com>
2020-11-11 17:46:44 +1000

    Add Mongoose and Create mongoDB connection to connect to MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI, {
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+
+        console.log(`Connect to MongoDB successfully at ${connection.connection.host}`);
+    } catch(error) {
+        console.log(`ERROR : ${error.message}`);
+    }
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
