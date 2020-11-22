bf02ed974a8536ae4ca451a1c744c2a2a8651f36
= <tranminhtri9090@gmail.com>
2020-11-22 12:04:48 +1000

    Adding create user route and hash the password before sending them to our databases

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,3 +1,4 @@
+import e from 'express';
 import AsyncHandler from 'express-async-handler';
 import User from '../models/user.js';
 import generateToken from '../utils/generateToken.js';
@@ -43,7 +44,42 @@ const getUserProfile = AsyncHandler(async (req, res) => {
     }
 }); 
 
+//?   @description : Create A New User 
+//?   @method : POST /api/users
+//?   @access : public 
+const createUser = AsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201);
+        res.json({
+            _id: user._id, 
+            name: user.name,
+            email: user.email,
+            isAdmin:  user.isAdmin,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+}); 
+
 export {
     login,
-    getUserProfile
+    getUserProfile,
+    createUser
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { login, getUserProfile } from '../controller/userController.js';
+import { login, getUserProfile, createUser } from '../controller/userController.js';
 import { protect } from '../middlewares/auth.js';
 
 router.route('/login').post(login);
+router.route('/').post(createUser);
 router.route('/profile').get(protect, getUserProfile);
 export default router;
