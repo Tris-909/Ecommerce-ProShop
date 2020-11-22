7fd38a0b8e52d7203b7170f8f85f6645fafda4cf
= <tranminhtri9090@gmail.com>
2020-11-22 17:30:53 +1000

    Adding Update User Info Profile Endpoint

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,3 @@
-import e from 'express';
 import AsyncHandler from 'express-async-handler';
 import User from '../models/user.js';
 import generateToken from '../utils/generateToken.js';
@@ -43,6 +42,35 @@ const getUserProfile = AsyncHandler(async (req, res) => {
     }
 }); 
 
+//?   @description : Change User Profile Information
+//?   @method : PUT /api/users/profile
+//?   @access : private 
+const changeUserProfile = AsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if ( req.body.password ) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id, 
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin:  updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        })
+    } else {
+        res.status(404);
+        throw new Error('This is not supposed to be happening');
+    }   
+}); 
+
 //?   @description : Create A New User 
 //?   @method : POST /api/users
 //?   @access : public 
@@ -79,5 +107,6 @@ const createUser = AsyncHandler(async (req, res) => {
 export {
     login,
     getUserProfile,
-    createUser
+    createUser,
+    changeUserProfile
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { login, getUserProfile, createUser } from '../controller/userController.js';
+import { login, getUserProfile, createUser, changeUserProfile } from '../controller/userController.js';
 import { protect } from '../middlewares/auth.js';
 
 router.route('/login').post(login);
 router.route('/').post(createUser);
-router.route('/profile').get(protect, getUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, changeUserProfile);
 export default router;
