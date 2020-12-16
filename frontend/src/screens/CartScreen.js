9af18032199d84c6c62d854f26dc1966e79c6e7f
= <tranminhtri9090@gmail.com>
2020-12-16 15:55:15 +1000

    Fix CartScreen Card Component make it look beautiful on mobile devices

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -4,6 +4,95 @@ import { Row, Col, ListGroup, Image, Button, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeItemFromCart, getAllItemsCart } from '../redux/actions/cartActions';
 import Helmet from '../components/Helmet';
+import styled from 'styled-components';
+
+//TODO: CART CARD 
+const CartCardItem = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    border: 1px solid grey;
+    margin-bottom: 1rem;
+    padding: 1rem; 
+
+    @media (max-width: 1050px) {
+        flex-direction: column;
+    }
+`;
+
+const CardImageContainer = styled.div`
+    width: 40%;
+
+    @media (max-width: 550px) {
+        width: 60%;
+    }
+`;
+
+const CardImage = styled.img`
+    width: 100%;
+`;
+
+const CardInfoContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const CardBottomContainer = styled.div`
+    margin-top: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const CardTextContainer = styled.div`
+    display: flex;
+    margin-left: 15px;
+    flex-direction: column;
+`;
+
+const CardProductName = styled.div`
+    font-size: 1rem;
+
+    @media (max-width: 400px) {
+        font-size: 0.8rem;
+    }
+`;
+
+const CardProductPrice = styled.div`
+    font-family: Rokkitt;
+    font-size: 1.5rem;
+`;
+
+const RemoveButton = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: underline;
+    cursor: pointer;
+`;
+//TODO: END OF CARD CART /////////////////////////////////
+
+const CartTitle = styled.div`
+    font-family: Rokkitt;
+    text-align: center;
+    font-size: 3rem;
+
+    @media (max-width: 500px) {
+        font-size: 2rem;
+    }
+`;
+
+const CartEmptyText = styled.div`
+    font-size: 2.5rem;
+    font-family: Rokkitt;
+    color: black;
+    text-align: center;
+    margin-top: 2rem;
+`;
 
 const CartScreen = ({ match, location, history }) => {
     const dispatch = useDispatch();
@@ -31,29 +120,33 @@ const CartScreen = ({ match, location, history }) => {
         <Row>
             <Helmet title='Your Shopping Cart' href='/cart' />
             <Col md={8}>
-                <h1>Shopping Cart</h1>
-                {cartItems.length === 0 ? <h2>Your Cart is empty <Link to="/">Go Back</Link></h2> : (
+                <CartTitle>Shopping Cart</CartTitle>
+                {cartItems.length === 0 ? <CartEmptyText>Your Cart is empty ! <Link to="/" style={{ textDecoration: 'underline' }}>Go Back</Link></CartEmptyText> : (
                     <ListGroup variant='flush'>
                         {cartItems.map(item => (
-                            <ListGroup.Item key={item._id}>
-                                <Row>
-                                    <Col md={4}>
-                                        <Image src={item.productImage} alt={item.productName} fluid rounded/>
-                                    </Col>
-                                    <Col md={4}>
-                                        <Link to={`/product/${item.itemId}`}>{item.productName}</Link>
-                                    </Col>
-                                    <Col md={2}>${item.productPrice}</Col>
-                                    <Col md={2}>
-                                        <Button 
-                                            type="button" 
-                                            variant="light"
-                                            onClick={() => removeFromCartHandler(item.itemId)}>
-                                                <i className='fas fa-trash'></i>
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
+                            <CartCardItem key={item._id}>
+                                <CardImageContainer>
+                                    <CardImage src={item.productImage} alt={item.productName} fluid rounded/>
+                                </CardImageContainer>
+                                    
+                                <CardInfoContainer>
+                                    <CardTextContainer>
+                                        <CardProductName>
+                                            <Link to={`/product/${item.itemId}`}>{item.productName}</Link>
+                                        </CardProductName>
+
+                                        <CardBottomContainer>
+                                            <CardProductPrice>
+                                                ${item.productPrice}
+                                            </CardProductPrice>
+
+                                            <RemoveButton onClick={() => removeFromCartHandler(item.itemId)}>
+                                                Remove                                        
+                                            </RemoveButton>
+                                        </CardBottomContainer>
+                                    </CardTextContainer>
+                                </CardInfoContainer>
+                            </CartCardItem>
                         ))}
                     </ListGroup>
                 )}
