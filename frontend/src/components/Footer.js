135ad1f09dce7baf51bd970d02f657eabcd00e82
= <tranminhtri9090@gmail.com>
2020-12-20 13:00:41 +1000

    Adding TypeScript and psuhing code before switching to TypeScript

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const FooterContainer = styled.div`
